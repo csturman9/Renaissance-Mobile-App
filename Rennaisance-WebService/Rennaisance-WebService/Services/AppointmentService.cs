e5ac633f1e7387ccfb8a86561cf6ae6ea4b52d6d
Cole Sturman <csturman@email.arizona.edu>
2017-04-27 14:23:37 -0700

    Testing Branch Functionality

---
+++ Rennaisance-WebService/Rennaisance-WebService/Services/AppointmentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rennaisance_WebService.Services
+{
+    public class AppointmentService
+    {
+        //TESTING BRANCH MERGING IN GITHUB
+    }
+}
